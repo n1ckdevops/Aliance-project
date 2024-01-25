c38d99d94324777d837812850b3e6168531bc90a
n1ckdevops <nickdevops6@gmail.com>
2024-01-25 19:35:22 +0300

    fixed smal bags

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ const logo = document.querySelector('.logo-svg use');
 window.addEventListener('scroll', () => { 
   if (this.scrollY > 1) {
     navbar.classList.add('navbar-light');
-    logo.href.baseVal = '../img/sprites.svg#logo';
+    logo.href.baseVal = 'img/sprites.svg#logo';
   } else {
     navbar.classList.remove('navbar-light');
-    logo.href.baseVal = '../img/sprites.svg#logo-white';
+    logo.href.baseVal = 'img/sprites.svg#logo-white';
   }
 });

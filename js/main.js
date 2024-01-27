0cebacbb1925a624fb70e5df0bbdfbcf0a387929
n1ckdevops <nickdevops6@gmail.com>
2024-01-28 02:05:12 +0300

    add: changes to flexible header

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-const navbar = document.querySelector('.navbar');
-const logo = document.querySelector('.main-logo-svg use');
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".main-logo-svg use");
+const menuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
 
-window.addEventListener('scroll', () => { 
+window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
-    navbar.classList.add('navbar-light');
-    logo.href.baseVal = 'img/sprites.svg#logo';
+    navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/sprites.svg#logo";
   } else {
-    navbar.classList.remove('navbar-light');
-    logo.href.baseVal = 'img/sprites.svg#logo-white';
+    navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprites.svg#logo-white";
   }
+});
+
+menuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle("is-open");
 });
